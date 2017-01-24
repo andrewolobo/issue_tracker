f219d118deff10cd4d101a8a88e5abeded7c9cf0
andrew.olobo@accessmobileinc.com <oloborama@gmail.com>
2017-01-24 18:24:00 +0300

    Initial Commit

---
+++ issue_tracker_one/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="issue_tracker_one.MvcApplication" Language="C#" %>
---
+++ issue_tracker_one/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(issue_tracker_one.Startup))]
+namespace issue_tracker_one
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
